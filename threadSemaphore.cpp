0e51c677824a244fa9cfbb8737e22f13accbbcb8
FZX <2298831252@qq.com>
2023-02-16 22:15:35 +0800

    [learn] semaphore Summary

---
+++ threadSemaphore.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<thread>
+
+//semaphore come with C++20
+
+using namespace std;
+
+int main(){
+    
+
+    return 0;
+}
---
+++ threadSummary.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<thread>
+#include<condition_variable>
+
+using namespace std;
+
+//neet to learn {future,promise,async}
+
+int main(){
+
+    return 0;
+}
